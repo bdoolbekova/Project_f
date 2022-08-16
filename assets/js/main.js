d008fcde965fe30ffaccdad33edc26fff3f3d6de
bermet <bdoolbekova@yahoo.com>
2022-08-16 16:11:25 -0400

    Modified

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,3 +20,28 @@ function linkAction(){
     navMenu.classList.remove('show')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+const sr= ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+})
+
+sr.reveal('.home_title',{})
+sr.reveal('.button',{delay: 200})
+sr.reveal('.home_img',{delay: 400})
+sr.reveal('.home_social-icon',{interval: 200})
+
+sr.reveal('.about_img',{})
+sr.reveal('.about_subtitle',{delay: 200})
+sr.reveal('.about_text',{delay: 400})
+
+sr.reveal('.skills_title',{})
+sr.reveal('.skills_text',{delay: 200})
+sr.reveal('.skills_data',{interval: 200})
+sr.reveal('.skils_img',{delay: 400})
+
+sr.reveal('.work_img',{interval: 200})
+
+sr.reveal('.contact_input',{interval: 200})
